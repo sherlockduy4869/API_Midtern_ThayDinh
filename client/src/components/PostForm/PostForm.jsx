6b0a633d8ff609e9422a02a98dec5ddf8eb5f2da
Khaug <huynhkhang690@gmail.com>
2022-11-22 18:09:33 +0700

    validate

--- client/src/components/PostForm/PostForm.jsx
+++ client/src/components/PostForm/PostForm.jsx
@@ -15,9 +15,11 @@ const PostForm = () => {
         image_food: ""
     })
 
+    const [showvalidation, setShowvalidation] = useState([])
+
     function submit(e) {
         e.preventDefault();
-        axios.post(url,{
+        axios.post(url, {
             name: data.name,
             unit_price: data.unit_price,
             promotion_price: data.promotion_price,
@@ -25,32 +27,38 @@ const PostForm = () => {
             description: data.description,
             image_food: data.image_food
         })
-        .then(res => {
-            console.log(res.data)
-        })
-        .catch(error => console.log(error['response']['data']['errors']));
-        
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(error => {
+                setShowvalidation(error['response']['data']['errors']['description'][0])
+            });
     }
 
+    console.log(showvalidation);
+
+
     function handle(e) {
-        const newdata = {...data}
+        const newdata = { ...data }
         newdata[e.target.id] = e.target.value
         setData(newdata)
         console.log(newdata)
     }
 
-    return(
+    return (
         <div className="app__post flex__center section__wrapper section__padding ">
-            <Heading title="Add Product"/>
+            <Heading title="Add Product" />
             <form className="app__post-form" onSubmit={(e) => submit(e)}>
                 <input onChange={(e) => handle(e)} id="name" value={data.name} placeholder="Name" type="text" ></input>
                 <input onChange={(e) => handle(e)} id="unit_price" value={data.unit_price} placeholder="Unit price" type="number" name="unit_price"></input>
                 <input onChange={(e) => handle(e)} id="promotion_price" value={data.promotion_price} placeholder="Promotion price" type="number" name="promotion_price"></input>
                 <input onChange={(e) => handle(e)} id="food_type" value={data.food_type} placeholder="Food type" type="text" name="food_type"></input>
                 <input onChange={(e) => handle(e)} id="description" value={data.description} placeholder="Description" type="text" name="description"></input>
+                <div>{showvalidation}</div>
                 <input onChange={(e) => handle(e)} id="image_food" value={data.image_food} placeholder="URL img" type="text" name="image"></input>
                 <button className="custom__button" type="submit">Submit</button>
             </form>
+            
         </div>
     )
 }
